b9463d1f189e4c2570a6b09720fc0d8ff98c28c0
rashmishrivastava <rashmi.gsti@gmail.com>
2021-01-20 22:37:56 +0100

    Code cleanup.

--- src/test/java/com/cognizant/csp/service/ValidatorTest.java
+++ src/test/java/com/cognizant/csp/service/ValidatorTest.java
@@ -7,16 +7,14 @@ import com.cognizant.csp.model.CustomerStatementProcessorResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(SpringExtension.class)
-@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class ValidatorTest {
 
     private Validator validator;
